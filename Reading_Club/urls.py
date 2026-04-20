ca6612aeb7cef2bf72203e5400fa5a5ce0b82a26
Arina M <sentinelmir@gmail.com>
2026-04-20 17:41:32 +0300

    fix: buges

--- Reading_Club/urls.py
+++ Reading_Club/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from Reading_Club.views import HomePageView
 from django.conf import settings
 from django.conf.urls.static import static
-
+from Reading_Club.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +12,9 @@ urlpatterns = [
     path('collection/', include('Reading_Club.collection.urls')),
     path('review/', include('Reading_Club.review.urls')),
     path('author/', include('Reading_Club.author.urls')),
-    path('api/', include('Reading_Club.api.urls'))
+    path('api/', include('Reading_Club.api.urls')),
+]
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
